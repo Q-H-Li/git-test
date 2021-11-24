39a90e88268c05f3179aad04faaa2643e779f406
Q-H-Li <77768951+Q-H-Li@users.noreply.github.com>
2021-11-24 19:48:59 +0800

    pull commit
    网页端修改代码

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello Git22344!! -- master test");
         System.out.println("hello Git22344!! -- hot-fix  test");
         System.out.println("push  test");
+        System.out.println("pull  test");
     }
 }
