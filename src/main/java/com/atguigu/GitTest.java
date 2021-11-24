f99dd9dcef52d015dc01d0dcf3fb85c04afcd038
QHLuck <821563014@qq.com>
2021-11-24 16:53:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author lUCK_L
+ * @Date 2021/11/2416:50
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!");
+    }
+}
