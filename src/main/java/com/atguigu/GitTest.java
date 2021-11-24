fde4551f94ccece95e5ef317b4e4a49e4cf3debf
QHLuck <821563014@qq.com>
2021-11-24 19:04:56 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello Git!!");
         System.out.println("hello Git223!!");
         System.out.println("hello Git22344!!");
+        System.out.println("hello Git22344!! -- hot-fix");
     }
 }
